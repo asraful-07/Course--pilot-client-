272ad369e0a9e14f40dbdb9c9c5bc7719d75b820
asraful-07 <asraful0508@gmail.com>
2025-04-23 18:41:25 +0600

    faq section done

--- src/app/(main-layout)/contact/page.jsx
+++ src/app/(main-layout)/contact/page.jsx
@@ -1,4 +1,5 @@
 import Contact from "@/app/components/Contact/Contact";
+import FAQ from "@/app/components/Contact/FAQ";
 import Map from "@/app/components/Contact/Map";
 import React from "react";
 
@@ -7,6 +8,7 @@ const page = () => {
     <div>
       <Map />
       <Contact />
+      <FAQ />
     </div>
   );
 };
