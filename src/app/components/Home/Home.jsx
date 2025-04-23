6f0978e40e98c0ef4b3cbda91da6118182f14259
asraful-07 <asraful0508@gmail.com>
2025-04-23 11:45:21 +0600

    discount section done

--- src/app/components/Home/Home.jsx
+++ src/app/components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../Banner";
 import Headline from "../Headline";
+import Discount from "../Discount";
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <Headline />
+      <Discount />
     </div>
   );
 };
