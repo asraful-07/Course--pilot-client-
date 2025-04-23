bec2f5be4868b2863ce716c22152498136a67458
asraful-07 <asraful0508@gmail.com>
2025-04-23 18:38:16 +0600

    map done

--- src/app/(main-layout)/contact/page.jsx
+++ src/app/(main-layout)/contact/page.jsx
@@ -1,9 +1,11 @@
 import Contact from "@/app/components/Contact/Contact";
+import Map from "@/app/components/Contact/Map";
 import React from "react";
 
 const page = () => {
   return (
     <div>
+      <Map />
       <Contact />
     </div>
   );
