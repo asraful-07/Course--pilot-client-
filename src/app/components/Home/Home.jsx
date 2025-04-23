ea58ddc665952af56b46ab1fb4610bfb01234a68
asraful-07 <asraful0508@gmail.com>
2025-04-23 11:42:03 +0600

    headline done

--- src/app/components/Home/Home.jsx
+++ src/app/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../Banner";
+import Headline from "../Headline";
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <Headline />
     </div>
   );
 };
