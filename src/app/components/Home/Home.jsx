7162157fddc7b51b46dbcb6854c9e1076794705d
asraful-07 <asraful0508@gmail.com>
2025-04-23 11:48:40 +0600

    review done

--- src/app/components/Home/Home.jsx
+++ src/app/components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Banner from "../Banner";
 import Headline from "../Headline";
 import Discount from "../Discount";
+import Review from "../Review";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Banner />
       <Headline />
       <Discount />
+      <Review />
     </div>
   );
 };
